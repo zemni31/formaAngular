28319cc3b030609f1bb621014853322b832ce578
zemni31 <rihab.zemni@insat.ucar.tn>
2025-06-15 17:25:28 +0100

    ajout de routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { Cv } from './CvProjet/cv/cv';
 import { Test } from './directive/test/test';
 export const routes: Routes = [
-    {path:'cv',component:Cv},
+    {path: 'cv', redirectTo: '/', pathMatch: 'full'},
+    {path:'',component:Cv},
     {path:'test',component:Test},
 ];
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet} from '@angular/router';
 
 import { Cv } from "./CvProjet/cv/cv";
 import { Test } from "./directive/test/test";
+import { Header } from "./header/header";
 
 
 @Component({
   selector: 'app-root',
    standalone: true,
-  imports: [Cv, Test,  RouterOutlet],
+  imports: [Cv, Test, RouterOutlet, Header],
   templateUrl: './app.html',
  // styleUrls: ['./app.component.css']
 })
