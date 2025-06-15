ab586c3d03d4c542948158e8c52ded6c3347a2f6
zemni31 <rihab.zemni@insat.ucar.tn>
2025-06-15 16:26:04 +0100

    mise en place d'un système de routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Cv } from './CvProjet/cv/cv';
+import { Test } from './directive/test/test';
+export const routes: Routes = [
+    {path:'cv',component:Cv},
+    {path:'test',component:Test},
+];
 
-export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { Cv } from "./CvProjet/cv/cv";
 import { Test } from "./directive/test/test";
@@ -7,7 +8,7 @@ import { Test } from "./directive/test/test";
 @Component({
   selector: 'app-root',
    standalone: true,
-  imports: [Cv, Test],
+  imports: [Cv, Test,  RouterOutlet],
   templateUrl: './app.html',
  // styleUrls: ['./app.component.css']
 })
