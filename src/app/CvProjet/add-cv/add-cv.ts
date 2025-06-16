d5eaad6fabd20bbadf7c8113661e1aa82fb80850
zemni31 <rihab.zemni@insat.ucar.tn>
2025-06-16 12:40:50 +0100

    form template driven

--- src/app/CvProjet/add-cv/add-cv.ts
+++ src/app/CvProjet/add-cv/add-cv.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FormsModule, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-add-cv',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-cv.html',
   styleUrl: './add-cv.css'
 })
 export class AddCv {
-
+addPersonne(formulaire: NgForm){
+  console.log(formulaire.value);
+}
 }
