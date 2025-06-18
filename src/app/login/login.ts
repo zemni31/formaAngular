920fa63bacb1fbcf83ab7aafffafcc5a99912830
zemni31 <rihab.zemni@insat.ucar.tn>
2025-06-18 08:38:35 +0100

    Accéder aux propriétés d’un champ du formulaire

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
 
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
